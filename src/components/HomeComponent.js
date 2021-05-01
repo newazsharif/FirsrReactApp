e4b249fd5125a053fb18f9d25911e1f4dd4ef0c8
Newaz <newaz@mobilityonegroup.com>
2021-05-02 00:55:42 +0600

    single page application part1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import {Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle} from 'reactstrap';
 
+
+function RenderCard({item}){
+    return(
+        <Card>
+            <CardImg src={item.image}></CardImg>
+            <CardBody>
+                <CardTitle>
+                    {item.name}
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                </CardTitle>
+                <CardText>
+                    {item.description}
+                </CardText>
+            </CardBody>
+        </Card>
+    );
+}
 function Home(props){
     return(
         <div className="contatiner">
-            <h2>Home</h2>
+            <div className="row align-items-start">
+                <div className = "col-12 col-md m-1">
+                    <RenderCard item = {props.dish} />
+                </div>
+                <div className = "col-12 col-md m-1">
+                    <RenderCard item = {props.promotion} />\
+                </div>
+                <div className = "col-12 col-md m-1">
+                    <RenderCard item = {props.leader} />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Redirect, Route, Switch } from 'react-router';
 import Home from './HomeComponent';
+import LEADERS from '../shared/Leaders';
+import PROMOTIONS from '../shared/Promotions';
 
 
 class Main extends Component{
@@ -12,12 +14,16 @@ class Main extends Component{
     super(props);
     this.state = {
       dishes : DISHES,
+      leaders : LEADERS,
+      promotions : PROMOTIONS
     };
   }
   render(){
     const HomePage = (props)=>{
       return(
-        <Home />
+        <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion = {this.state.promotions.filter((promotion) => promotion.featured)[0]}
+        leader = {this.state.leaders.filter((leader) => leader.featured)[0]} />
       )
     }
     return (
